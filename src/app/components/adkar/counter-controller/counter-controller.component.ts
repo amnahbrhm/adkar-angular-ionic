a54c6a59e7947490782c294dbb534a7380318643
Marwa <koroko123430@gmail.com>
2022-09-24 10:30:46 +0300

    انشاء وظيفة العداد

--- src/app/components/adkar/counter-controller/counter-controller.component.ts
+++ src/app/components/adkar/counter-controller/counter-controller.component.ts
@@ -3,12 +3,24 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-counter-controller',
   templateUrl: './counter-controller.component.html',
-  styleUrls: ['./counter-controller.component.scss','../morning-adkar/morning-adkar.component.scss'],
+  styleUrls: ['./counter-controller.component.scss', '../morning-adkar/morning-adkar.component.scss'],
 })
 export class CounterControllerComponent implements OnInit {
   @Input() count: number;
-  constructor() { }
+  counter = 0;
+  constructor() {
+  }
+  ngOnInit() {
+    this.reset();
+  }
+  decrease() {
+    if (this.counter > 0) {
+      this.counter--;
+    }
+  }
+  reset() {
+    this.counter = this.count;
+  }
 
-  ngOnInit() { }
 
 }
